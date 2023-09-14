95a9fe117c8cde5285d17f6c35d89515578976bf
Aman <aman.mangoit@gmail.com>
2023-09-14 19:45:04 +0530

    Added category dynamicly on categories component

--- src/screens/admin/categories/Index.js
+++ src/screens/admin/categories/Index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {
   FlatList,
@@ -9,70 +9,41 @@ import {
   View,
   Image,
 } from 'react-native';
+
+import { getCategoriesApi } from '../../../api/auth';
 import MyTabs from '../../../components/Bottomnavigation';
-import {getCategoriesApi} from '../../../api/auth';
 
 const Categories = () => {
   const [selectedCategory, setSelectedCategory] = useState(Select);
   const [categoryList, setCategoryList] = useState([]);
+  const [onClick, setOnClick] = useState(false);
   // console.log('Data', categoryList);
-
-  const Data = ['Women', 'Men', 'Kids'];
-  const Select = 'Women';
-  const cateArr = [];
-  // console.log('newArray', cateArr);
-
-  //------------ Get the parentId
-  const getData = categoryList.map(
-    item => (
-      {
-        parent: item.parentId,
-        id: item.id,
-      }
-    ),
-  );
-
-  getData.map(item => {
-    if (item.parent == null) {
-      const idd = item.id;
-      console.log('idddd', idd);
-    }
-  });
-
-  //---- Seperated the parentId in two variables Null and Numbers
-  // const nullValues = [];
-  // const numberValues = [];
-
-  // getData.forEach(value => {
-  //   if (value === null) {
-  //     nullValues.push(value);
-  //   } else {
-  //     numberValues.push(value);
-  //   }
-  // });
-
-  // console.log("ppp", nullValues);
-  // console.log("ppp", numberValues);
-
-  //  if(numberValues === 72 && categoryList === 72 ){
-  //   cateArr =  categoryList
-  // }
-
-  // categoryList.map(item => {
-  //   const data = item.categoryTranslations
-  //   // console.log('ssdsadsd', data);
-  //   for (let i = 0; i < data.length; i++) {
-  //     const obj = data[i];
-  //     // console.log("objData",obj);
-  //     // cateArr.push(obj)
-  //   }
-  // });
 
   useEffect(() => {
     getCategoriesApi().then(result => {
       setCategoryList(result?.result?.data?.rows);
     });
   }, []);
+
+  const Select = 'Apparels';
+  const ab = categoryList.filter(val => val && val.parentId === null);
+
+  const parIdNotNull = categoryList
+    .filter(val => val && val.parentId !== null)
+    .map(val => val.categoryTranslations).flat()
+    .map(val => val.name)
+
+  const firstFive = parIdNotNull.slice(0, 12);
+
+  const catData = ab
+    .map(val => val.categoryTranslations)
+    .flat()
+    .map(newdata => newdata.name);
+
+
+  const handlePress = () => {
+     setOnClick(true)
+  }
 
   return (
     <>
@@ -85,17 +56,17 @@ const Categories = () => {
             marginTop: 20,
           }}>
           <Icon
-            style={{marginLeft: 13}}
+            style={{ marginLeft: 13 }}
             name="arrow-back-ios"
             color="black"
             size={40}
           />
-          <Text style={{marginTop: 5, fontSize: 25, color: 'black'}}>
+          <Text style={{ marginTop: 5, fontSize: 25, color: 'black' }}>
             Categories
           </Text>
           <TouchableOpacity>
             <Icon
-              style={{marginRight: 10}}
+              style={{ marginRight: 10 }}
               name="search"
               color="black"
               size={40}
@@ -106,24 +77,25 @@ const Categories = () => {
         <FlatList
           style={[styles.flatlist]}
           horizontal
-          data={Data}
+          data={catData}
           showsHorizontalScrollIndicator={false}
-          renderItem={({item}) => {
+          renderItem={({ item }) => {
             return (
-              <TouchableOpacity
-                style={[Select ? styles.selectedItemContainer : {}]}>
-                <Text
-                  style={[
-                    styles.text,
-                    selectedCategory ? styles.selectedItemContainer : {},
-                  ]}>
-                  {item}
-                </Text>
-              </TouchableOpacity>
+              <>
+                <TouchableOpacity onPress={handlePress}
+                  style={[Select ? styles.selectedItemContainer : {}]}>
+                  <Text
+                    style={[
+                      styles.text,
+                      selectedCategory ? styles.selectedItemContainer : {},
+                    ]}>
+                    {item}
+                  </Text>
+                </TouchableOpacity>
+              </>
             );
           }}
         />
-
         <View style={[styles.saleView]}>
           <Text style={[styles.saleText]}>SUMMER SALES</Text>
           <Text style={[styles.discount]}>Up to 50% off</Text>
@@ -182,6 +154,7 @@ const Categories = () => {
   );
 };
 export default Categories;
+
 
 const styles = StyleSheet.create({
   flatlist: {
@@ -265,3 +238,145 @@ const styles = StyleSheet.create({
     top: -22.5,
   },
 });
+
+// seeData.map((item) => (
+//   <FlatList
+//     data={item}
+//     renderItem={({ item }) => {
+//       console.log("data", item);
+//       return (
+//           <ul>
+//             <li>{item.name}</li>
+//           </ul>
+//       );
+//     }}
+//   />
+// )
+
+// const categoriArray = [];
+
+// const filteredData = categoryList.map(
+//   product => product && product.parentId);
+// // console.log("fff", filteredData);
+
+// const parentIds = categoryList.map(item => ({
+//   data: {
+//     parentId: item.parentId,
+//     id: item.id
+//   },
+// }));
+
+// console.log("pppp",parentIds);
+
+//   const newData = parentIds.map((item, index) => ({
+//     [`data${index + 1}`]: item.data
+//   }));
+//  console.log("newnewe", newData);
+
+// const category = categoryList.map(res =>(res.categoryTranslations)).flat()
+// console.log("rerere", category);
+
+// const parentIdsSet = new Set(parentIds);
+// const parentIds = categoryList.reduce((result, item) => {
+//   if (!parentIdsSet.has(item.parentId)) {
+//     parentIdsSet.add(item.parentId);
+//     result.push({
+//       data: {
+//         parentId: item.parentId,
+//         id: item.id
+//       }
+//     });
+//   }
+//   return result;
+// }, []);
+// console.log("afafaf", parentIds);
+
+// console.log("pppp",parentIdsSet);
+// const cateArr = [];
+// console.log('newArray', cateArr);
+
+// const getData = categoryList.map(item => ({
+//   parent: item.parentId,
+//   id: item.id,
+// }));
+
+// const fetchId = getData
+//   .filter(item => item.parent === null)
+//   .map(item => item.id);
+// console.log('newId', fetchId);
+
+// const getName = fetchId.map(id => {
+//   const category = Object.values(categoryList).find(product => {
+//     return (
+//       product.categoryTranslations &&
+//       product.categoryTranslations.some(
+//         translation => translation.categoryId === id,
+//       )
+//     );
+//   });
+//   return {
+//     id: id,
+//     name:
+//       category && category.categoryTranslations
+//         ? category.categoryTranslations[0].name
+//         : null,
+//   };
+// });
+// console.log("ssddsdsd", getName);
+
+// console.log("dsdsf", getData);
+// const cateData = categoryList.map(item => item.categoryTranslations)
+//   .flat()
+//   .map((item) => {
+//     if (item.categoryId && getData.id == 72 ) {
+//       cateArr = item
+//       console.log("aaaaaaa",cateArr);
+//     }
+//     return item
+//   }
+//   )
+// console.log("cateData", cateData);
+
+// ------------ Get the parentId
+
+//---- Seperated the parentId in two variables Null and Numbers
+// const nullValues = [];
+// const numberValues = [];
+
+// getData.forEach(value => {
+//   if (value.parent === null) {
+//     nullValues.push(value);
+//   } else {
+//     numberValues.push(value);
+//   }
+// });
+
+// const data = categoryList.map((item) => {
+//   if (item.parentId === null) {
+//     if(item.categoryTranslations){
+//       return item;
+//     }
+//   }
+// });
+// console.log("dssdsf", data);
+
+// const matchingNullValue = nullValues.find((nullValue) => nullValue.parent);
+// console.log("dssdsf", matchingNullValue);
+
+//  if(numberValues === 72 && categoryList === 72 ){
+//   cateArr =  categoryList
+// }
+
+// categoryList.map(item => {
+//   const data = item.categoryTranslations
+//   // console.log('ssdsadsd', data);
+//   for (let i = 0; i < data.length; i++) {
+//     const obj = data[i];
+//     // console.log("objData",obj);
+//     // cateArr.push(obj)
+//   }
+// });
+
+// const handlePress = () => {
+//   setTab(!mytabs);
+// };
